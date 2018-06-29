6834464b86f1d43159777f5e2e304fa96fe9c299
Kelly Stratton <polytopek@gmail.com>
2018-06-28 20:50:33 -0600

    updated tracking

---
+++ src/reducers/reducer_chrome_storage.js
@@ -0,0 +1,18 @@
+import { INIT, SAVE_TO_STORAGE } from '../actions';
+
+const DEFAULT_STATE = {
+	colors: [],
+	selected: '',
+};
+
+export default function(state = DEFAULT_STATE, action) {
+	console.log('received action:', action);
+	switch (action.type) {
+		case INIT:
+			return Object.assign({}, state, action.payload);
+		case SAVE_TO_STORAGE:
+			return Object.assign({}, state, action.payload);
+	}
+
+	return state;
+}
