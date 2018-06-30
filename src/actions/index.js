3008ff6f8bede60e9d4901ff5bed7a6bad219a5f
Kelly Stratton <polytopek@gmail.com>
2018-06-30 14:44:37 -0600

    Added conditional logic to chrome actions to facilitate development-mode

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@ export const INIT = 'INIT';
 export const SAVE_TO_STORAGE = 'SAVE_TO_STORAGE';
 export const REMOVE_FROM_STORAGE = 'REMOVE_FROM_STORAGE';
 
+import { default_state } from '../chrome_extension/default_state';
+
 function emitChromeStorageAction(data, type) {
 	return {
 		type: type,
@@ -10,20 +12,32 @@ function emitChromeStorageAction(data, type) {
 }
 
 export const initFromChromeStorage = () => {
-	return dispatch => {
-		chrome.storage.sync.get('state', data => {
-			dispatch(emitChromeStorageAction(data.state, INIT));
-		});
-	};
+	if (chrome.storage) {
+		return dispatch => {
+			chrome.storage.sync.get('state', data => {
+				dispatch(emitChromeStorageAction(data.state, INIT));
+			});
+		};
+	} else {
+		return dispatch => {
+			dispatch(emitChromeStorageAction(default_state, INIT));
+		};
+	}
 };
 
 export const saveToChromeStorage = (key, value) => {
-	return dispatch => {
-		chrome.storage.sync.get('state', data => {
-			const newState = Object.assign({}, data.state, { [key]: value });
-			chrome.storage.sync.set({ state: newState }, () => {
-				dispatch(emitChromeStorageAction({ [key]: value }, SAVE_TO_STORAGE));
+	if (chrome.storage) {
+		return dispatch => {
+			chrome.storage.sync.get('state', data => {
+				const newState = Object.assign({}, data.state, { [key]: value });
+				chrome.storage.sync.set({ state: newState }, () => {
+					dispatch(emitChromeStorageAction({ [key]: value }, SAVE_TO_STORAGE));
+				});
 			});
-		});
-	};
+		};
+	} else {
+		return dispatch => {
+			dispatch(emitChromeStorageAction({ [key]: value }, SAVE_TO_STORAGE));
+		};
+	}
 };
